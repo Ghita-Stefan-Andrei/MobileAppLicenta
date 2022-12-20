99971483ed9874917ede6f4fcc74dcbb7027544f
Ghita Stefan-Andrei <stefststef72@gmail.com>
2022-12-20 19:10:26 +0200

    added imap setup to mainactivity

--- src/app/src/main/java/com/ghitastefanandrei/mywatermeter/MainActivity.kt
+++ src/app/src/main/java/com/ghitastefanandrei/mywatermeter/MainActivity.kt
@@ -3,6 +3,9 @@ package com.ghitastefanandrei.mywatermeter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ghitastefanandrei.mywatermeter.databinding.ActivityMainBinding
+import java.util.*
+import javax.mail.*
+//import java.net.Authenticator
 
 class MainActivity : AppCompatActivity()
 {
@@ -15,6 +18,34 @@ class MainActivity : AppCompatActivity()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.value.text = "00065500"
+        val email:String = "esp32camLicenta@gmail.com"
+        val password:String = "mcgd paai veol lssr"
+
+        val properties = Properties().apply {
+            put("mail.store.protocol", "imap")
+            put("mail.imap.host", "imap.gmail.com")
+            put("mail.imap.port", "993")
+            put("mail.imap.ssl.enable", "true")
+        }
+
+        val session = Session.getInstance(properties)
+        val store = session.getStore("imap")
+
+        store.connect(email, password)
+/*
+        val inbox = store.getFolder("INBOX")
+        inbox.open(Folder.READ_ONLY)
+
+        val messages = inbox.messages
+
+        for (message in messages) {
+            binding.mSubject.text = message.subject
+            Thread.sleep(2000)
+        }
+
+        inbox.close(false)
+        store.close()*/
     }
+
+
 }
